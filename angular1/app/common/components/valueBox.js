c404e229a354fadeefafc613d7df56f2d55d99e2
Evair Carvalho <evair.carvalho27@gmail.com>
2017-08-07 22:46:12 -0300

    ValueBox and GridSystem

---
+++ angular1/app/common/components/valueBox.js
@@ -0,0 +1,28 @@
+angular.module('primeiraApp').component('valueBox', {
+	bindings: { /*entradas*/
+		grid: '@',
+		colorClass: '@',
+		value: '@',
+		text: '@',
+		iconClass: '@',
+	},
+	controller: [ /*controle*/
+		'gridSystem',
+		function(gridSystem) { /*Só funciona quando os bindings estiverem inicializados*/
+			this.$onInit = () => this.gridClasses = gridSystem.toCssClasses(this.grid)			
+		}
+	],
+	template: /*Template*/ ` 
+	<div class="{{ $ctrl.gridClasses }}">
+		<div class="small-box {{ $ctrl.colorClass }}">
+			<div class="inner">
+				<h3>{{ $ctrl.value }}</h3>
+				<p> {{ $ctrl.text }} </p>
+			</div>
+			<div class="icon">
+				<i class="fa {{ $ctrl.iconClass }}"></i>
+			</div>
+		</div>
+	</div>
+	`
+})
