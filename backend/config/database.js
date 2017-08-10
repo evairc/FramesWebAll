52094cd4d48133b2a25f4c1d9e3de4c42edf916c
Evair Carvalho <evair.carvalho27@gmail.com>
2017-08-10 18:04:39 -0300

    Factory Msgs and Show Tabs

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose'); /*responsável pela comunicação do mongo
 
 module.exports = mongoose.connect('mongodb://localhost/db_finance');
 
-mongoose.Error.messages.general.required = "O atributo '{PATH} é obritatório."
+mongoose.Error.messages.general.required = "O atributo '{PATH} é obrigatório."
 
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
 mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite mínimo de '{MAX}'."
